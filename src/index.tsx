d7085ef2135152bbd5718d2842fc309eba4e815d
Yasin ULUSOY <yasin.ulusoy@yahoo.com>
2023-10-06 15:36:26 +0300

    feat: project created

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+ReactDOM.render(
+    <div>
+        <h1>Project created</h1>
+    </div>,
+    document.getElementById("root")
+);
